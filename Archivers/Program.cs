4dfe0bf4835cd469eb65ef3986c29a56215a0a9d
AdIVIuH <42569158+AdIVIuH@users.noreply.github.com>
2021-11-17 22:39:17 +0500

    UI

--- Archivers/Program.cs
+++ Archivers/Program.cs
@@ -14,46 +14,55 @@ namespace Archivers
             Console.WriteLine("2. Use all archivers;");
 
             Console.WriteLine("Please, choose an input method:");
-            Console.WriteLine("1. Just past to the console input;");
+            Console.WriteLine("1. Just put in to the console input;");
             Console.WriteLine("2. Input from the file.");
             var inputMethod = Console.ReadLine();
             var entry = string.Empty;
+
+            if (inputMethod == "1")
+                entry = Console.ReadLine();
+
             if (inputMethod == "2")
             {
                 Console.WriteLine("Please, enter file path.");
 
                 var path = Console.ReadLine();
-                using (var fstream = File.OpenRead(path))
-                {
-                    // преобразуем строку в байты
-                    var array = new byte[fstream.Length];
-                    // считываем данные
-                    fstream.Read(array, 0, array.Length);
-                    // декодируем байты в строку
-                    entry = System.Text.Encoding.Default.GetString(array);
-                    Console.WriteLine($"Text from the file: {entry}");
-                }
+                using var fstream = File.OpenRead(path);
+                // преобразуем строку в байты
+                var array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                entry = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine($"Text from the file: {entry}");
             }
 
             var lzw = new LZW();
 
             var compressed = lzw.Compress(entry);
             var decompressed = lzw.Decompress(compressed);
-            Console.WriteLine("Compressed: " + compressed);
-            Console.WriteLine("Decompressed: " + decompressed);
-
-            using (var fstream = new FileStream($@"C:\Users\kvash\Desktop\archiveComressed.txt", FileMode.OpenOrCreate))
+            if (inputMethod == "1")
             {
-                fstream.Write(compressed.Select(el => (byte)el).ToArray(), 0, compressed.Count);
-                Console.WriteLine("Текст compressed записан в файл");
+                Console.WriteLine("Compressed: " + compressed);
+                Console.WriteLine("Decompressed: " + decompressed);
             }
 
-            using (var fstream =
-                new FileStream($@"C:\Users\kvash\Desktop\archiveDecomressed.txt", FileMode.OpenOrCreate))
+            if (inputMethod == "2")
             {
-                var array = System.Text.Encoding.Default.GetBytes(decompressed);
-                fstream.Write(array, 0, array.Length);
-                Console.WriteLine("Текст decompressed записан в файл");
+                using (var fstream =
+                    new FileStream($@"C:\Users\kvash\Desktop\archiveComressed.txt", FileMode.OpenOrCreate))
+                {
+                    fstream.Write(compressed.Select(el => (byte)el).ToArray(), 0, compressed.Count);
+                    Console.WriteLine("Text compressed and saved to the file archiveComressed.txt");
+                }
+
+                using (var fstream = new FileStream($@"C:\Users\kvash\Desktop\archiveDecomressed.txt",
+                    FileMode.OpenOrCreate))
+                {
+                    var array = System.Text.Encoding.Default.GetBytes(decompressed);
+                    fstream.Write(array, 0, array.Length);
+                    Console.WriteLine("Text decompressed and saved to the file archiveDecomressed.txt");
+                }
             }
         }
     }
